79b4257e0f87c04c8696bb2b52b6f56013cc20ff
Arushi Somani <arushisomani@berkeley.edu>
2022-03-13 23:57:36 -0700

    fixing

--- static/js/song.js
+++ static/js/song.js
@@ -1,9 +1,12 @@
-fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${process.env.SPOTIFY_USER}&api_key=${process.env.LASTFM_KEY}&limit=1&nowplaying=true&format=json`)
+let userName="amksomani";
+let apiKey="80f6445a8f5cb6abcde40082838af940";
+
+
+fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${userName}&api_key=${apiKey}&limit=1&nowplaying=true&format=json`)
  .then(response => response.json())
  .then(data => {
     let song = data.recenttracks.track[0]['name'];
     let artist = data.recenttracks.track[0]['artist']['#text'];
-    let more_info = crunchData(data);
     let text = `At this very moment, I'm listening to "${song}" by ${artist}. Yes, I listen to a lot of pop.`;
 
     let songTitle = document.getElementById('song');
